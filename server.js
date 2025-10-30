d13f5c6972facca56b5995dc14188a6da353f203
Shantannu gupta <145862065+ShantannuDev2003@users.noreply.github.com>
2025-10-30 20:12:10 +0530

    Fix CORS origin URL by removing trailing slash

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://password-manager-usfv.vercel.app/"
+    "https://password-manager-usfv.vercel.app"
   ],
   methods: ["GET", "POST", "DELETE"],
   allowedHeaders: ["Content-Type"]
