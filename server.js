928bff91ff6fd09d0fed1f73608d636e35d68731
Shantannu gupta <145862065+ShantannuDev2003@users.noreply.github.com>
2025-10-30 20:53:34 +0530

    Update CORS origin for password manager

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://password-manager-usfv-b0hh3ctvs-shantannus-projects.vercel.app"
+    "https://password-manager-usfv.vercel.app"
   ],
   methods: ["GET", "POST", "DELETE"],
   allowedHeaders: ["Content-Type"]
